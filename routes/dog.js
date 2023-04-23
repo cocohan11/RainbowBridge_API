37502d62b566b787cf03d39cc0dab9cd3c5c0a45
root <root@ip-172-31-47-11.ap-northeast-2.compute.internal>
2023-04-23 11:19:51 +0000

    add others

--- routes/dog.js
+++ routes/dog.js
@@ -27,9 +27,9 @@ class CommonError extends Error {}
 
 // AWS 접근키 설정
 AWS.config.update({
-  region: config.region,
-  accessKeyId: config.accessKeyId,
-  secretAccessKey: config.secretAccessKey
+ region: config.region,
+ accessKeyId: config.accessKeyId,
+ secretAccessKey: config.secretAccessKey
 });
 
 const s3 = new AWS.S3();
