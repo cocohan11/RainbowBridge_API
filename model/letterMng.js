4293581b2dc6478182c606f27298cbfbe2cc6d0c
han2 <rlagksdl96@naver.com>
2023-06-29 03:16:58 +0900

    0628) 한: 편지조회, 엽서 작성, 엽서조회 API 개발완료

--- model/letterMng.js
+++ model/letterMng.js
@@ -18,7 +18,7 @@ letterMng.prototype.mySQLQuery = (query) => {
                   return reject(err);
               } else {
                   //순차적으로 실행하면 반환되는 행을 관리
-                  return resolve(camelcaseKeys(rows));
+                  return resolve(rows);
               }
           });
       } catch (err) {
@@ -46,11 +46,64 @@ function mySQLQuery(query) {
 };
 
 
+//DB에 작성한 엽서 SELECT
+letterMng.prototype.selectPostcards = (query) => {
+    
+
+  // 쿼리: DOG_POSTCARD 테이블에서 이전에 강아지가 보낸 엽서에 대한 정보를 조회한다.
+  function selectPostcard(query) {
+    return {
+      text: `SELECT * FROM DOG_POSTCARD 
+              WHERE user_id = ? and postcard_id = ? `, 
+      params: [query.user_id, query.postcard_id] 
+    };
+  }
+  function selectPostcards(query) {
+    return {
+      text: `SELECT * FROM DOG_POSTCARD 
+              WHERE user_id = ?`, 
+      params: [query.user_id] 
+    };
+  }
+
+
+  // 엽서 여러장이라면
+  if (query.postcard_id == undefined || query.postcard_id == null) {
+    // 쿼리 실행 결과, 편지id를 리턴한다. 
+    return new Promise((resolve, reject) => {
+      mySQLQuery(selectPostcards(query)) 
+      .then((res) => { 
+        logger.warn(`엽서 res : ${res}`);
+        return resolve(res);
+      }) 
+      .catch((err) => {
+        logger.warn(`selectLetter() err: ${err} `);
+        return resolve(9999); 
+      });
+    }); 
+  } else {
+    // 쿼리 실행 결과, 편지id를 리턴한다. 
+    return new Promise((resolve, reject) => {
+      mySQLQuery(selectPostcard(query)) 
+      .then((res) => { 
+        logger.warn(`엽서 res : ${res}`);
+        return resolve(res);
+      }) 
+      .catch((err) => {
+        logger.warn(`selectLetter() err: ${err} `);
+        return resolve(9999); 
+      });
+  }); 
+  }
+}
+
+
+
 //DB에 작성한 편지 SELECT
-letterMng.prototype.selectLetter = (query) => {
+letterMng.prototype.selectLetters = (query) => {
     
     // 쿼리: MEMBER_LETTER 테이블에서 이전에 강아지에게 보냈던 편지에 대한 정보를 조회한다.
-    function selectMemberLetters(query) {
+    function selectLetters(query) {
         return {
           text: `SELECT * FROM MEMBER_LETTER 
                  WHERE user_id = ?`, 
@@ -60,9 +113,8 @@ letterMng.prototype.selectLetter = (query) => {
 
     // 쿼리 실행 결과, 편지id를 리턴한다. 
     return new Promise((resolve, reject) => {
-        mySQLQuery(selectMemberLetters(query)) 
+        mySQLQuery(selectLetters(query)) 
         .then((res) => { 
-            
           logger.warn(`편지 res : ${res}`);
           return resolve(res);
         }) 
@@ -92,8 +144,8 @@ letterMng.prototype.insertNewLetter = (query) => {
     return new Promise((resolve, reject) => {
         mySQLQuery(insertMember(query)) 
         .then((res) => { 
-          logger.warn(`편지id : ${res.letter_id}`);
-          return resolve(res.letter_id); // res.letter_id : insert한 row의 id를 얻는다.
+          logger.info(`편지id : ${res.insertId}`);
+          return resolve(res.insertId); // res.insert_id : insert한 row의 id를 얻는다.
         })
         .catch((err) => {
           logger.warn(`insertNewLetter() err: ${err} `);
@@ -102,6 +154,36 @@ letterMng.prototype.insertNewLetter = (query) => {
     }); 
     
 }
+
+
+//DB에 작성된 엽서 INSERT
+letterMng.prototype.insertNewPostcard = (query) => {
+    
+  // 쿼리: DOG_POSTCARD 테이블에 강아지에게 받은 엽서에 대한 정보를 삽입한다.
+  function insertPostcard(query) {
+    return {
+      text: `INSERT INTO DOG_POSTCARD 
+              (postcard_id, user_id, dog_id, letter_content, img_path, date) 
+              VALUES (?, ?, ?, ?, ?, NOW())`, 
+      params: [query.postcard_id, query.user_id, query.dog_id, query.letter_content, query.img_path, query.date] 
+    };
+  }
+
+  // 쿼리 실행 결과, 편지id를 리턴한다. 
+  return new Promise((resolve, reject) => {
+      mySQLQuery(insertPostcard(query)) 
+      .then((res) => { 
+        logger.warn(`엽서id : ${res.insertId}`);
+        return resolve(res.insertId); // res.insertId : insert한 row의 id를 얻는다.
+      })
+      .catch((err) => {
+        logger.warn(`insertNewPostcard() err: ${err} `);
+      return resolve(9999); 
+      });
+  }); 
+  
+}
+
   
 
 //letterMng 모듈 export 
--- routes/letter.js
+++ routes/letter.js
@@ -68,7 +68,7 @@ router.get('/readLetter/:user_id?', async (req, res) => {
         return resCode.returnResponseCode(res, 1002, apiName, null, null);
 
     // DB - select문
-    const rows = await letterMngDB.selectLetter(req.params.user_id);
+    const rows = await letterMngDB.selectLetters(req.params.user_id);
     logger.info(`selectLetter() 리턴값: \n${JSON.stringify(rows, null, 2)}`);
 
     // 응답코드 0000 : 성공
@@ -83,10 +83,40 @@ router.get('/readLetter/:user_id?', async (req, res) => {
 
 
 /**
- * 편지작성 API 
- * @route {POST} api/letter/write
+ * 엽서조회 API 
+ * @route {GET} api/letter/readPostcard/:user_id
+ * 
  */
-router.post('/write', async (req, res) => {
+router.get('/readPostcard/:user_id?/:postcard_id?', async (req, res) => {
+
+
+  const apiName = '엽서조회';
+  logger.info(`${apiName} API`);
+  logger.info(`user_id : ${req.params.user_id}`);
+  logger.info(`postcard_id : ${req.params.postcard_id}`);
+
+  // 응답코드 1002 : 필수파라미터 누락 오류
+  if (!req.params.user_id) // postcard_id 는 필수아님
+      return resCode.returnResponseCode(res, 1002, apiName, null, null);
+
+  // DB - select문
+  const rows = await letterMngDB.selectPostcards(req.params);
+  logger.info(`selectLetter() 리턴값: \n${JSON.stringify(rows, null, 2)}`);
+
+  // 응답코드 0000 : 성공
+  if (rows != null && rows != undefined) {
+      return resCode.returnResponseCode(res, 0, apiName, 'arraylist', rows);
+      
+  // 응답코드 9999 : 기타 에러 코드
+  } else {
+      return resCode.returnResponseCode(res, 9999, apiName, null, null);
+  }
+})
+/**
+ * 편지작성 API 
+ * @route {POST} api/letter/writeLetter
+ */
+router.post('/writeLetter', async (req, res) => {
 
 
     const apiName = '편지작성';
@@ -111,6 +141,39 @@ router.post('/write', async (req, res) => {
         return resCode.returnResponseCode(res, 0, apiName, "addToResult", plusResult);
     }
 })
+
+
+
+/**
+ * 엽서작성 API 
+ * @route {POST} api/letter/writePostcard
+ */
+router.post('/writePostcard', async (req, res) => {
+
+
+  const apiName = '엽서작성';
+  logger.info(`${apiName} API`);
+  logger.info(`POST 바디: \n${JSON.stringify(req.body, null, 2)}`);
+
+  // 응답코드 1002 : 필수파라미터 누락 오류
+  if (!req.body.user_id || !req.body.dog_id || !req.body.letter_content || !req.body.img_path) 
+      return resCode.returnResponseCode(res, 1002, apiName, null, null);
+
+  // DB - insert문
+  const result = await letterMngDB.insertNewPostcard(req.body);
+  logger.info(`result : ${result}`);
+
+  // 응답코드 9999 : 기타 에러 코드
+  if (result == 9999) {
+      return resCode.returnResponseCode(res, 9999, apiName, null, null);
+  
+  // 응답코드 0000 : 성공
+  } else {
+      const plusResult = { postcard_id: result }; // 원하는 출력 모양을 만듦
+      return resCode.returnResponseCode(res, 0, apiName, "addToResult", plusResult);
+  }
+})
+
 
 
 module.exports = router;
